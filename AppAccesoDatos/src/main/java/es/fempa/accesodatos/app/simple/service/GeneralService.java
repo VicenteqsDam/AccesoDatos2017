4f3c47b00daed1a1a7115ab9e6d7f356b1f0ae90
Miguel <miguelverdu1812@gmail.com>
2017-09-27 18:06:14 +0200

    acabandp

--- AppAccesoDatos/src/main/java/es/fempa/accesodatos/app/simple/service/GeneralService.java
+++ AppAccesoDatos/src/main/java/es/fempa/accesodatos/app/simple/service/GeneralService.java
@@ -16,10 +16,20 @@
 
 package es.fempa.accesodatos.app.simple.service;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Properties;
 
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
 import org.springframework.stereotype.Service;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 @Service
 public class GeneralService {
@@ -29,6 +39,54 @@ public class GeneralService {
 	}
 
 	public ArrayList<Properties> createDemoList() {
+
+		DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+		factory.setIgnoringComments(true);
+		factory.setIgnoringElementContentWhitespace(true);
+		DocumentBuilder builder = null;
+		try {
+			builder = factory.newDocumentBuilder();
+		} catch (ParserConfigurationException e) {
+			e.printStackTrace();
+		}
+		File file = new File("src\\main\\resources\\files\\alumnos.xml");
+		Document doc = null;
+		try {
+			doc = builder.parse(file);
+		} catch (SAXException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Node raiz = doc.getFirstChild();
+		System.out.println("todo correcto");
+
+		NodeList nl = raiz.getChildNodes();
+
+		for (int i = 0; i < nl.getLength(); i++) {
+			System.out.println(nl.item(i).getNodeName());
+			if (nl.item(i).getNodeName().equals("ROW")) {
+				if (nl.item(i).hasChildNodes()) {
+					NodeList aux = nl.item(i).getChildNodes();
+					
+					for (int j = 0; j < aux.getLength(); j++) {
+						if(!aux.item(j).getNodeName().equals("#text")){
+						System.out.print(aux.item(j).getNodeName()+": ");
+
+						
+						if (aux.item(j).hasChildNodes()) {
+							if(!aux.item(j).getNodeName().equals("#text")){
+							System.out.println(aux.item(j).getFirstChild().getNodeValue());
+						}
+						}
+					}
+				}
+			}
+
+		}
+
 		ArrayList<Properties> list = new ArrayList<Properties>();
 		Properties p = new Properties();
 		p.put("name", "Vicente");
